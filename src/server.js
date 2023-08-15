831b972b30f1b97717add7108f5d96ca41d9cbc7
Soham Malviya <sohammalviya65@gmail.com>
2023-08-15 10:50:50 +0530

    / route implemented

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  console.log("hello from Express");
+  res.status(200);
+  res.json({ message: "hello" });
+});
+
+module.exports = app;
