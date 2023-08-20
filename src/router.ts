2b83457b4fd4d5c1a0d4463add6ec16a49ff150f
Soham Malviya <sohammalviya65@gmail.com>
2023-08-20 10:38:34 +0530

    API routes structures for /product & /update

---
+++ src/router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+router.get("/update", () => {});
+router.get("/update:id", () => {});
+router.put("/update:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
