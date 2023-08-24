bb80cf9714acd1ffbec609579d4b2bcad2099549
Soham Malviya <sohammalviya65@gmail.com>
2023-08-24 17:16:49 +0530

    bug fix

--- src/modules/middleware.ts
+++ src/modules/middleware.ts
@@ -2,7 +2,7 @@ import { validationResult } from "express-validator";
 
 export const handleInputErrors = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty) {
+  if (!errors.isEmpty()) {
     res.status(400);
     res.json({ errors: errors.array() });
   } else {
