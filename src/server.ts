4b3a62c943d6fabd4d20232ea397cbf6c3ddc34d
Soham Malviya <sohammalviya65@gmail.com>
2023-09-04 22:45:12 +0530

    error handler

--- src/server.ts
+++ src/server.ts
@@ -23,4 +23,14 @@ app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signIn);
 
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ messahe: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "invalid input" });
+  } else {
+    res.status(500).json({ message: "Oopsie, that's on us" });
+  }
+});
+
 export default app;
