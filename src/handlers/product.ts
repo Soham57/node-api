a075dd49161a69aebd3bfbb2b420e8383f5cefc3
Soham Malviya <sohammalviya65@gmail.com>
2023-09-04 12:10:14 +0530

    adding Unique index for product id and belongsToId

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -49,3 +49,13 @@ export const updateProduct = async (req, res) => {
   });
   res.json({ data: updated });
 };
+
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id: req.params.id,
+      belongsToId: req.user.id,
+    },
+  });
+  res.json({ data: deleted });
+};
