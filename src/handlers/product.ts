f1641c7e49d592678ef7f19d893c5bd6ee45a799
Soham Malviya <sohammalviya65@gmail.com>
2023-09-04 11:27:58 +0530

    getting all products

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,3 +1,14 @@
 import prisma from "../db";
 
-export const getProducts = async (req, res) => {};
+export const getProducts = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  res.json({ data: user.product });
+};
