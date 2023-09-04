c63c172ec4130c024f5d0cf202b5d277b54d3157
Soham Malviya <sohammalviya65@gmail.com>
2023-09-04 08:17:09 +0530

    Isolating error handler logic

--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,39 @@
 import { Router } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 router.get("/product", (req, res) => {
   res.json({ message: "Hello" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 
-  if (!errors.isEmpty()) {
-    res.status(400);
-    res.json({ errors: errors.array() });
-  }
-});
-router.post("/product", () => {});
 router.delete("/product/:id", () => {});
 
 router.get("/update", () => {});
 router.get("/update:id", () => {});
 router.put("/update:id", () => {});
-router.post("/update", () => {});
+router.post(
+  "/update",
+  body("title").isString(),
+  body("body").isString(),
+  body("asset").isString(),
+  body("productId").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 router.delete("/update/:id", () => {});
 
 export default router;
